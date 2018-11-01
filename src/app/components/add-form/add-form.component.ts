d9a72a03dda8e30b84984f20555585626aa4866f
sophievillerius <sophievillerius@gmail.com>
2018-11-01 14:41:47 +0100

    added post function for videos via the form

--- src/app/components/add-form/add-form.component.ts
+++ src/app/components/add-form/add-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/youtube-movie';
+import { YoutubeService } from '../../services/youtube.service'
 
 @Component({
   selector: 'app-add-form',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFormComponent implements OnInit {
 
-  constructor() { }
+  movie = new Movie;
+
+  constructor(
+    private youtubeService: YoutubeService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  submit(title: string, youTubeId: string, description: string) {
+    this.youtubeService.submitMovie(title, youTubeId, description);
   }
 
 }
