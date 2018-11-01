8c22db98cd487a61f14140954b8aa28bbda446fe
jalvaros <35036415+jalvaros@users.noreply.github.com>
2018-11-01 07:37:14 +0100

    [ADD] banner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { VideoCardComponent } from './components/video-card/video-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomepageComponent},
+  {path: "videos", component: VideoCardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,24 @@ import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FoodComponent } from './components/food/food.component';
 import { YoutubeComponent } from './components/youtube/youtube.component';
 import { SafepipePipe } from './pipes/safepipe.pipe';
-import { FilmslistComponent } from './components/filmslist/filmslist.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { VideoCardComponent } from './components/video-card/video-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FoodComponent,
     YoutubeComponent,
     SafepipePipe,
-    FilmslistComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BannerComponent,
+    HomepageComponent,
+    VideoCardComponent
   ],
   imports: [
     BrowserModule,
